a195586d565cd1baef16d6f181ae2211d5ff72c2
Sneha <Sneha@DESKTOP-K46N7GD>
2026-02-10 18:46:02 +0530

    Added window-size in chromeoptions browserutility file

--- src/test/java/com/automationframework/utilities/BrowserUtilities.java
+++ src/test/java/com/automationframework/utilities/BrowserUtilities.java
@@ -30,6 +30,7 @@ public abstract class BrowserUtilities {
 			if (isHeadless) {
 				ChromeOptions chromeOptions=new ChromeOptions();
 				chromeOptions.addArguments("--headless");
+				chromeOptions.addArguments("--window-size=1920,1080");
 				driverLocal.set(new ChromeDriver(chromeOptions));
 
 			} else {
